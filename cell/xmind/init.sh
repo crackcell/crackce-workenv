4fb4bea2ed2848104e46a516dbe80fb7184ad866
Menglong TAN <tanmenglong@gmail.com>
2015-06-14 00:28:40 +0800

    add xmind

---
+++ cell/xmind/init.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+##! @description: usage
+##! @version: 1
+##! @author: crackcell <tanmenglong@gmail.com>
+##! @date:   Sun Jun 14 00:25:43 2015
+
+export $PATH="$ENV_HOME/lib/xmind/default/XMind_Linux_64bit/:$PATH"
